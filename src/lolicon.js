fdb3a8dbc759c6fb4f44537841abaec5425bd85e
Larry Jackson <45368772+seaguli@users.noreply.github.com>
2020-07-21 05:28:41 -0400

    Create lolicon.js

---
+++ src/lolicon.js
@@ -0,0 +1,25 @@
+/**
+ *  Lolicon Main
+ *
+ * Me me big disappointment
+ */
+import LoliconException from './LoliconException';
+var Lolicon;
+(function (Lolicon) {
+    class IDoables {
+        Intice() {
+            //do nothing because weebs are already attracted to underage anime girls
+            return null;
+        }
+        callTheFBI(IDangerous) {
+            if (IDangerous.__is_danger = true) {
+                child_process.exec('call_911', stdout => {
+                    return stdout;
+                });
+            }
+            else {
+                return LoliconException.Error('probably just something weird happened.');
+            }
+        }
+    }
+})(Lolicon || (Lolicon = {}));
